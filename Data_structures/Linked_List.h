ba93de4881cae4d54ca09381906f84b613d77fbd
JustTaya <37045184+JustTaya@users.noreply.github.com>
2018-03-13 19:23:44 +0200

    Linked list

--- Data_structures/Linked_List.h
+++ Data_structures/Linked_List.h
@@ -1,149 +1,151 @@
 #pragma once
 #include <iostream>
 
-template <class T>
-struct TNode
-{
-	T dat;
-	TNode<T>* next = NULL;
-};
+namespace structures {
+	template <class T>
+	struct TNode_l
+	{
+		T data;
+		TNode_l<T>* next = NULL;
+	};
 
-template <class T>
-class Linked_List {
-private:
-	TNode<T> *begnode = NULL;
-	TNode<T> *headnode = NULL;
-	bool empty;
-public:
-	TNode<T> *it = NULL;
-	Linked_List();
-	~Linked_List();
-	bool SetIterator(size_t pos);
-	T ReturnVal();
-	bool PushBack(T data);
-	bool Insert(size_t pos, T data);
-	bool DelElement(size_t pos);
-	size_t Numb();
-	TNode<T>* End();
-};
+	template <class T>
+	class LinkedList {
+	public:
+		TNode_l<T> *it = NULL;
+		LinkedList();
+		~LinkedList();
+		bool setIterator(size_t position);
+		T returnVal();
+		bool pushBack(T inputData);
+		bool insert(size_t position, T inputData);
+		bool delElement(size_t position);
+		size_t numb();
+		TNode_l<T>* end();
+	private:
+		TNode_l<T> *begnode = NULL;
+		TNode_l<T> *headnode = NULL;
+		bool empty;
+	};
 
-template<class T>
-Linked_List<T>::Linked_List()
-{
-	this->begnode = NULL;
-	this->headnode = this->begnode;
-	empty = true;
-	this->it = this->begnode;
-}
-
-template<class T>
-Linked_List<T>::~Linked_List()
-{
-	TNode<T>*node;
-	while (begnode != NULL) {
-		node = this->begnode;
-		this->begnode = (this->begnode)->next;
-		delete (node);
+	template<class T>
+	LinkedList<T>::LinkedList()
+	{
+		this->begnode = NULL;
+		this->headnode = this->begnode;
+		empty = true;
+		this->it = this->begnode;
 	}
-}
 
-template<class T>
-inline bool Linked_List<T>::SetIterator(size_t pos)
-{
-	this->it = this->begnode;
-	for (size_t i = 0; i < pos - 1; i++)
-		this->it = (this->it)->next;
-	return false;
-}
+	template<class T>
+	LinkedList<T>::~LinkedList()
+	{
+		TNode_l<T>*node;
+		while (begnode != NULL) {
+			node = this->begnode;
+			this->begnode = (this->begnode)->next;
+			delete (node);
+		}
+	}
 
-template<class T>
-T Linked_List<T>::ReturnVal()
-{
-	TNode<T> *node = this->it;
-	this->it = it->next;
-	return T(node->dat);
-}
-
-template<class T>
-bool Linked_List<T>::PushBack(T data)
-{
-	TNode<T>* tmp = new TNode<T>();
-	tmp->dat = data;
-	if (empty) {
-		empty = false;
-		this->begnode = tmp;
-		this->headnode = tmp;
-		this->it = tmp;
+	template<class T>
+	bool LinkedList<T>::setIterator(size_t position)
+	{
+		this->it = this->begnode;
+		for (size_t i = 0; i < position - 1; i++)
+			this->it = (this->it)->next;
 		return false;
 	}
-	(this->headnode)->next = tmp;
-	this->headnode = tmp;
-	return false;
-}
 
-template<class T>
-bool Linked_List<T>::Insert(size_t pos, T data)
-{
-	TNode<T>*node = new TNode<T>();
-	node->dat = data;
-	if (pos == 1) {
-		node->next = this->begnode;
-		begnode = node;
-		return true;
+	template<class T>
+	T LinkedList<T>::returnVal()
+	{
+		TNode_l<T> *node = this->it;
+		this->it = it->next;
+		return T(node->data);
 	}
-	else {
-		TNode<T>* tmp = this->begnode;
-		for (size_t i = 0; i < pos - 2 && tmp->next; i++)
-			tmp = tmp->next;
-		node->next = tmp->next;
-		tmp->next = node;
-		if (node->next == NULL)
-			this->headnode = node;
-		return true;
-	}
-	return false;
-}
 
-template<class T>
-bool Linked_List<T>::DelElement(size_t pos)
-{
-	TNode<T>*tmp = this->begnode;
-	if (pos == 1) {
-		this->begnode = (this->begnode)->next;
-		delete tmp;
-		return true;
+	template<class T>
+	bool LinkedList<T>::pushBack(T inputData)
+	{
+		TNode_l<T>* tmp = new TNode_l<T>();
+		tmp->data = inputData;
+		if (empty) {
+			empty = false;
+			this->begnode = tmp;
+			this->headnode = tmp;
+			this->it = tmp;
+			return false;
+		}
+		(this->headnode)->next = tmp;
+		this->headnode = tmp;
+		return false;
 	}
-	else {
-		for (size_t i = 0; i < pos - 2; i++) {
+
+	template<class T>
+	bool LinkedList<T>::insert(size_t position, T inputData)
+	{
+		TNode_l<T>*node = new TNode_l<T>();
+		node->data = inputData;
+		if (position == 1) {
+			node->next = this->begnode;
+			begnode = node;
+			return true;
+		}
+		else {
+			TNode_l<T>* tmp = this->begnode;
+			for (size_t i = 0; i < position - 2 && tmp->next; i++)
+				tmp = tmp->next;
+			node->next = tmp->next;
+			tmp->next = node;
+			if (node->next == NULL)
+				this->headnode = node;
+			return true;
+		}
+		return false;
+	}
+
+	template<class T>
+	bool LinkedList<T>::delElement(size_t position)
+	{
+		TNode_l<T>*tmp = this->begnode;
+		if (position == 1) {
+			this->begnode = (this->begnode)->next;
+			delete tmp;
+			return true;
+		}
+		else {
+			for (size_t i = 0; i < position - 2; i++) {
+				if (tmp->next == NULL)
+					return false;
+				tmp = tmp->next;
+			}
+			TNode_l<T>*del = tmp->next;
+			tmp->next = (tmp->next)->next;
+			delete del;
 			if (tmp->next == NULL)
-				return false;
+				this->headnode = tmp;
+			return true;
+		}
+		return false;
+	}
+
+	template<class T>
+	size_t LinkedList<T>::numb()
+	{
+		TNode_l<T>* tmp = this->begnode;
+		size_t k = 1;
+		while (tmp != this->headnode)
+		{
+			k++;
 			tmp = tmp->next;
 		}
-		TNode<T>*del = tmp->next;
-		tmp->next = (tmp->next)->next;
-		delete del;
-		if (tmp->next == NULL)
-			this->headnode = tmp;
-		return true;
+		return k;
 	}
-	return false;
-}
 
-template<class T>
-size_t Linked_List<T>::Numb()
-{
-	TNode<T>* tmp = this->begnode;
-	size_t k = 1;
-	while (tmp != this->headnode)
+	template<class T>
+	TNode_l<T>* LinkedList<T>::end()
 	{
-		k++;
-		tmp = tmp->next;
+		return (this->headnode);
 	}
-	return k;
-}
-
-template<class T>
-TNode<T>* Linked_List<T>::End()
-{
-	return (this->headnode);
 }
