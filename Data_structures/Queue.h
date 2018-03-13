8195d9902d23eacff987b9977a9880a4658a07a9
JustTaya <37045184+JustTaya@users.noreply.github.com>
2018-03-13 19:21:53 +0200

    Queue

--- Data_structures/Queue.h
+++ Data_structures/Queue.h
@@ -1,83 +1,85 @@
 #pragma once
 #include <iostream>
 
-template <class T>
-struct QNode {
-	QNode<T> *next_node=nullptr;
-	T dat;
-};
+namespace structures {
+	template <class T>
+	struct TNode_q
+	{
+		T data;
+		TNode_q<T>* next = NULL;
+	};
+	template <class T>
+	class Queue {
+	public:
+		Queue();
+		~Queue();
+		void enqueue(T  inputData);
+		T dequeue();
+		bool empty();
+		size_t size();
+	private:
+		TNode_q<T> *head_;
+		TNode_q<T> *tail_;
+		size_t size_;
+	};
 
-template <class T>
-class Queue {
-private:
-	QNode<T> *head;
-	QNode<T> *tail;
-	size_t size;
-public:
-	Queue();
-	~Queue();
-	bool Enqueue(T  data);
-	T Dequeue();
-	bool Empty();
-	size_t Size();
-};
-
-template<class T>
-inline Queue<T>::Queue()
-{
-	head = nullptr;
-	tail = head;
-	size = 0;
-}
-
-template<class T>
-Queue<T>::~Queue()
-{
-	QNode<T>* tmp;
-	while (this->head) {
-		tmp = this->head;
-		this->head = this->head->next_node;
-		delete tmp;
+	template<class T>
+	inline Queue<T>::Queue()
+	{
+		head_ = nullptr;
+		tail_ = head_;
+		size_ = 0;
 	}
-}
 
-template<class T>
-bool Queue<T>::Enqueue(T data)
-{
-	QNode<T>* tmp = new QNode<T>();
-	tmp->dat = data;
-	size++;
-	if (this->head==nullptr) {
-		this->head = tmp;
-		this->tail = tmp;
+	template<class T>
+	Queue<T>::~Queue()
+	{
+		TNode_q<T>* tmp;
+		while (this->head_) {
+			tmp = this->head_;
+			this->head_ = this->head_->next;
+			delete tmp;
+		}
+	}
+
+	template<class T>
+	void Queue<T>::enqueue(T inputData)
+	{
+		TNode_q<T>* tmp = new TNode_q<T>();
+		tmp->data = inputData;
+		size_++;
+		if (this->head_ == nullptr) {
+			this->head_ = tmp;
+			this->tail_ = tmp;
+		}
+		else {
+			(this->tail_)->next = tmp;
+			this->tail_ = tmp;
+		}
+	}
+
+	template<class T>
+	T Queue<T>::dequeue()
+	{
+		TNode_q<T>*tmp = this->head_;
+		T outputData = this->head_->data;
+		this->head_ = this->head_->next;
+		size_--;
+		delete tmp;
+		return outputData;
+	}
+
+	template<class T>
+	bool Queue<T>::empty()
+	{
+		if (this->head_ == nullptr)
+			return true;
 		return false;
 	}
-	(this->tail)->next_node = tmp;
-	this->tail = tmp;
-	return false;
-}
 
-template<class T>
-T Queue<T>::Dequeue()
-{
-	QNode<T>*tmp = this->head;
-	T data = this->head->dat;
-	this->head = this->head->next_node;
-	size--;
-	delete tmp;
-	return data;
-}
-
-template<class T>
-bool Queue<T>::Empty()
-{
-	if (this->head == nullptr)
-		return true;
-	return false;
-}
-
-template<class T>
-inline size_t Queue<T>::Size()
-{
-	return size;
+	template<class T>
+	size_t Queue<T>::size()
+	{
+		return size_;
+	}
 }
